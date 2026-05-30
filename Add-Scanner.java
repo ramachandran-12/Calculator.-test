daeef0b5c34f90cd29effa4442b50ecba598530f
ramachandran-12 <ramlaksh053@gmail.com>
2026-05-30 16:50:48 +0530

    add-Scanner

---
+++ Add-Scanner.java
@@ -0,0 +1,9 @@
+static void add (int x, int y) {
+		result = x+y;
+		System.out.println("The add result is " +result);
+	}
+Scanner sc = new Scanner(System.in);
+		int x =sc.nextInt();
+		System.out.println("Enter the number");
+		int y = sc.nextInt();
+		SimpleCalcul.add(x, y);
